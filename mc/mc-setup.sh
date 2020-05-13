aa1c206d6be4f99930b12455916adbd87443240e
Mikhail Fomichev <mikhail@fomichev.xyz>
2020-05-13 10:20:50 +0300

    add setup for mc

---
+++ mc/mc-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm /usr/local/Cellar/midnight-commander/4.8.24/etc/mc/mc.ext
+ln -sfv $PWD/mc.ext /usr/local/Cellar/midnight-commander/4.8.24/etc/mc/mc.ext
+
--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ ln -sfv $PWD/functions $HOME/.functions
 source ./brew-packages-setup.sh
 
 source ./nvim/nvim-setup.sh
+source ./mc/mc-setup.sh
 
 ln -sfv $PWD/tmux/tmux.conf $HOME/.tmux.conf
 
