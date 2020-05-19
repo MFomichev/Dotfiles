7f4428443a4b4904d8141e8a6c0b1f4b1e86143f
Mikhail Fomichev <mikhail@fomichev.xyz>
2020-05-19 09:36:43 +0300

    fix zsh link

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ ln -sfv $PWD/functions $HOME/.functions
 
 source ./brew-packages-setup.sh
 
+source ./zshrc/zsh-setup.sh
 source ./nvim/nvim-setup.sh
 source ./mc/mc-setup.sh
 
-ln -sfv $PWD/zshrc $HOME/.zshrc
 ln -sfv $PWD/tmux/tmux.conf $HOME/.tmux.conf
 
