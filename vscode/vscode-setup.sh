95152d15d6037f27be84d0799fb1673785f5956b
Mikhail Fomichev <mikhail@fomichev.xyz>
2020-07-07 14:40:06 +0300

    install redhat.java extention for vscod via brew

--- vscode/vscode-setup.sh
+++ vscode/vscode-setup.sh
@@ -2,3 +2,11 @@
 
 ln -sfv $PWD/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 
+EXTENTIONS=(
+  redhat.java
+)
+
+for extension in ${EXTENTIONS[@]};do
+  code --install-extension --force $extension
+done
+
