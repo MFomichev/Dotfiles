75ba14522ae92c3e658e12dc5f076f85d8396824
Mikhail Fomichev <mikhail@fomichev.xyz>
2020-07-25 17:30:27 +0300

    install MRD via mas

--- mas.sh
+++ mas.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 APPS=(
-  585829637 #Todoist
-  409183694 #Keynote
-  409203825 #Numbers
-  441258766 #Magne0t
+  585829637  #Todoist
+  409183694  #Keynote
+  409203825  #Numbers
+  441258766  #Magnet
+  1295203466 #Microsoft Remote Desktop
 )
 
 for app in ${APPS[@]};do
