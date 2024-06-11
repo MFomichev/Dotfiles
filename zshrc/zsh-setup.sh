39240ec25e78745d45bb28360494e40f1e2450d8
Mikhail Fomichev <mikhail@fomichev.xyz>
2024-06-11 22:29:35 +0300

    change files names

--- zshrc/zsh-setup.sh
+++ zshrc/zsh-setup.sh
@@ -2,7 +2,7 @@
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-ln -sfv $PWD/zshrc/.zprofile $HOME/.zprofile
-ln -sfv $PWD/zshrc/.zshrc $HOME/.zshrc
-ln -sfv $PWD/zshrc/.aliases $HOME/.aliases
+ln -sfv $PWD/zshrc/zprofile $HOME/.zprofile
+ln -sfv $PWD/zshrc/aliases $HOME/.aliases
+ln -sfv $PWD/zshrc/zshrc $HOME/.zshrc
 
