dd8a5be8caafa08b02ec9bff5f9fd5bf731c7dc0
Mikhail Fomichev <mikhail@fomichev.xyz>
2024-06-11 18:50:07 +0300

    fix brew

--- zshrc/zsh-setup.sh
+++ zshrc/zsh-setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+ln -sfv $PWD/zshrc/.zprofile $HOME/.zprofile
 ln -sfv $PWD/zshrc/.zshrc $HOME/.zshrc
 ln -sfv $PWD/zshrc/.aliases $HOME/.aliases
 
