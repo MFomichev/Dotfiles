74ac0a8ae899e190bcb98faf1dac7ceff422db51
Mikhail Fomichev <mikhail@fomichev.xyz>
2024-07-05 20:53:23 +0300

    Make iterm2 sync via setting file

--- iterm2/iterm2-setup.sh
+++ iterm2/iterm2-setup.sh
@@ -2,5 +2,9 @@
 
 brew install --cask iterm2
 
-ln -sfv $PWD/iterm2.plist $HOME/Library/Preferences
+# Specify the preferences directory
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$PWD/iterm2/settings"
+
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
 
