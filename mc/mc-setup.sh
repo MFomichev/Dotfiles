eb718cfc5faa06fe8ccc8d3d7ea871d989682630
Mikhail Fomichev <mikhail@fomichev.xyz>
2024-07-12 16:10:28 +0300

    fix mc config

--- mc/mc-setup.sh
+++ mc/mc-setup.sh
@@ -2,6 +2,6 @@
 
 mkdir -p $HOME/.config/mc
 
-ln -sfv $PWD/mc.ext $HOME/.config/mc/mc.ext
-ln -sfv $PWD/ini $HOME/.config/mc/ini
+ln -sfv $PWD/mc/mc.ext $HOME/.config/mc/mc.ext
+ln -sfv $PWD/mc/ini $HOME/.config/mc/ini
 
